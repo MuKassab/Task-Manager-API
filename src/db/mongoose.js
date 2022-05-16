44fe3f4be9ce06b13024a2430265cfe108be6fc0
Muhammad M. Kassab <muhammadkassab97@yahoo.com>
2022-05-16 13:29:02 +0200

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const fs = require('fs')
 
+//Connection to MongooseDB
+//URL is stored as an environment variable for security
 mongoose.connect(process.env.MONGODB_URL.toString(), {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true})
-
-
-
